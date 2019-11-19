08a68271d2c29fab03f6ebab0b6cf67b686dad1e
develop3344 <56298005+develop3344@users.noreply.github.com>
2019-11-19 17:29:34 +0800

    Create 11911.c

---
+++ 11911.c
@@ -0,0 +1,19 @@
+/* 判断n位数（n>=3）是否为 水仙花数 */
+# include<stdio.h>
+int main (void)
+{
+    int n,sum=0,term，tmp；
+    printf("Enter n:\n");
+    scanf("%d",&n);
+    
+    for()
+    while(n>0)
+    {
+        term= tmp%10;
+        sum += term;
+        n= n/10;
+    }
+
+    printf("Sum is %d\n",sum);
+    return 0;
+}
