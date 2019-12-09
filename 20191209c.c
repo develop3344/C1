7305af91d46b907227d8055de8621bebb54b8304
develop37 <56298005+develop37@users.noreply.github.com>
2019-12-09 09:03:34 +0800

    Create 20191209c.c

---
+++ 20191209c.c
@@ -0,0 +1,29 @@
+#include <stdio.h>//精简版本
+
+int isPrime(int x)
+
+{
+    int i;
+    for (i = 2; i * i <= x; i++)
+    {
+        if (x % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(void)
+{
+    int i, flag;
+    for (i = 2; i <= 100; i++)
+    {
+        flag = isPrime(i);
+        if (flag == 1)
+        {
+            printf("%5d", i);
+        }
+    }
+    return 0;
+}
