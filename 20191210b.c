e5d2f21ce4075423dbff1949ca6c40dce3c44d97
develop37 <56298005+develop37@users.noreply.github.com>
2019-12-10 17:23:59 +0800

    Update 20191210b.c

--- 20191210b.c
+++ 20191210b.c
@@ -17,3 +17,26 @@ int main(void)
     }
     return 0;
 }
+
+
+
+
+#include <stdio.h>
+#define LEN 4   //用LEN 或者 N
+
+int main(void)
+{
+    int a[LEN], i;   
+
+    printf("Enter array a:\n");
+    for (i = 0; i <LEN; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = LEN-1; i >=0; i--) //反序输出
+    {
+        printf("%5d", a[i]);
+    }
+    return 0;
+}
