bae27e074a60b517a9b89045bf0d60b827d68a01
develop37 <56298005+develop37@users.noreply.github.com>
2019-12-16 09:26:51 +0800

    Create 20191216b.c

---
+++ 20191216b.c
@@ -0,0 +1,37 @@
+// 交换排序 从大到小
+#include <stdio.h>
+#define LEN 5
+
+int main(void)
+{
+    int a[LEN], max, i;
+    printf("Enter an array:\n");
+    for (i = 0; i < LEN; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    sort(a, LEN); //注意
+    for (i = 0; i < LEN; i++)
+    {
+        printf("%5d", a[i]);
+    }
+
+    return 0;
+}
+
+void sort(int x[], int n) //为什么为 void
+{
+    int i, j, tmp;
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (x[j] > x[i])
+            {
+                tmp = x[j];     //tmp 中间变量
+                x[j] = x[i];
+                x[i] = tmp;
+            }
+        }
+    }
+}
