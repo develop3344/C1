98db4720f821b8ca2df0eb40a81fca52b841e5ef
develop37 <56298005+develop37@users.noreply.github.com>
2019-12-17 17:36:30 +0800

    Update 20191217a.c

--- 20191217a.c
+++ 20191217a.c
@@ -24,3 +24,31 @@ int main (void)
         printf("\n");//换行 两行三列
     }
 }
+
+
+//二维数组
+#include<stdio.h>
+#define N 2
+#define M 3
+
+int main (void)
+{
+    int a[N][M],i,j;
+    printf("Enter an array:\n");
+    for(i=0;i<N;i++)
+    {
+        for(j=0;j<M;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    for(i=0;i<M;i++)
+    {
+        for(j=0;j<N;j++)          //M，N互换
+        {
+           printf("%5d",a[j][i]);      //i,j互换   转置
+        }
+        printf("\n");//换行 两行三列
+    }
+}
